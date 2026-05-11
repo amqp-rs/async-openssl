9cb6d831801229605e8a513ea85e39fe73e3c41e
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2026-05-11 23:30:12 +0200

    rustfmt
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@
 //! blocking [`Read`] and [`Write`] traits.
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 #![warn(unreachable_pub, unused_qualifications, unused_lifetimes)]
-#![warn(clippy::must_use_candidate, clippy::unwrap_in_result, clippy::panic_in_result_fn)]
+#![warn(
+    clippy::must_use_candidate,
+    clippy::unwrap_in_result,
+    clippy::panic_in_result_fn
+)]
 
 use futures_io::{AsyncRead, AsyncWrite};
 use openssl::{
